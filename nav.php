35a2d991c0ed8962371ad400c3ab5177dc4e7433
KEVN <troostkevin8@gmail.com>
2023-02-20 16:06:48 +0100

    html turnback

--- nav.php
+++
@@ -1,4 +0,0 @@
-<ul>
-    <img class="arkev" src="pictures/THElogo.png" onclick="window.location.href='index.html'" width="100" height="35">
-    <a class="cta" href="searchcontact.html"><button>Contact</button></a>
-</ul>
